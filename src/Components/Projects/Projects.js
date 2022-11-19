9ebafbb1929a7323b1a726f09e046563a5b28b67
Selim Ellieh <selimellieh@Selims-MBP.lan>
2022-11-19 14:54:32 +0200

    Added distancia mobile

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,7 +3,7 @@ import Project from '../Project/Project'
 import './Projects.scss'
 
 const PROJECTS = [
-    {name: 'DistanciaMobile', description: 'Digitalizing education', github_link: 'https://github.com/selimellieh72/DistanciaMobile', technology: 'Flutter'},
+    {name: 'DistanciaMobile', description: 'A cross-platform app implementation of Distancia ~ Digitalizing education', github_link: 'https://github.com/selimellieh72/DistanciaMobile', technology: 'Flutter'},
     {name: 'TicTactoeWeb', description: 'Play TicTacToe against an unbeatable AI!', github_link: 'https://github.com/selimellieh72/TicTacToeWeb', technology: 'JavaScript'},
     {name: 'My Plans', description: 'A cross-platform app to manage your plans created with Flutter and Hive.', github_link: 'https://github.com/selimellieh72/my-plans/', technology: 'Flutter'},
     {name: 'Project', description: 'Digitalizing education'}

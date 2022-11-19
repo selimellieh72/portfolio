0ebfff43c3951456a0c2619d201771b6b307617d
Selim Ellieh <selimellieh@Selims-MBP.lan>
2022-11-19 14:49:40 +0200

    Added project DistanciaMobile

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,9 +3,9 @@ import Project from '../Project/Project'
 import './Projects.scss'
 
 const PROJECTS = [
+    {name: 'DistanciaMobile', description: 'Digitalizing education', github_link: 'https://github.com/selimellieh72/DistanciaMobile', technology: 'Flutter'},
     {name: 'TicTactoeWeb', description: 'Play TicTacToe against an unbeatable AI!', github_link: 'https://github.com/selimellieh72/TicTacToeWeb', technology: 'JavaScript'},
     {name: 'My Plans', description: 'A cross-platform app to manage your plans created with Flutter and Hive.', github_link: 'https://github.com/selimellieh72/my-plans/', technology: 'Flutter'},
-    {name: 'Project', description: 'Digitalizing education'},
     {name: 'Project', description: 'Digitalizing education'}
 ]
 export default function Projects() {

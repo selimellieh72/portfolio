905271ab8f1330dc8c84c6cf8409a09af1111f1b
Selim Ellieh <selimellieh@Selims-MBP.lan>
2022-12-24 22:33:34 +0200

    Added Distancia Web

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,21 +1,44 @@
-import React from 'react'
-import Project from '../Project/Project'
-import './Projects.scss'
+import React from "react";
+import Project from "../Project/Project";
+import "./Projects.scss";
 
 const PROJECTS = [
-    {name: 'DistanciaMobile', description: 'A cross-platform app implementation of Distancia ~ Digitalizing education', github_link: 'https://github.com/selimellieh72/DistanciaMobile', technology: 'Flutter'},
-    {name: 'TicTactoeWeb', description: 'Play TicTacToe against an unbeatable AI!', github_link: 'https://github.com/selimellieh72/TicTacToeWeb', technology: 'JavaScript'},
-    {name: 'My Plans', description: 'A cross-platform app to manage your plans created with Flutter and Hive.', github_link: 'https://github.com/selimellieh72/my-plans/', technology: 'Flutter'},
-    {name: 'Project', description: 'Digitalizing education'}
-]
+  {
+    name: "Distancia",
+    description: "Digitalizing education",
+    github_link: "https://github.com/selimellieh72/Distancia",
+    technology: "JavaScript",
+  },
+  {
+    name: "DistanciaMobile",
+    description:
+      "A cross-platform app implementation of Distancia ~ Digitalizing education",
+    github_link: "https://github.com/selimellieh72/DistanciaMobile",
+    technology: "Flutter",
+  },
+  {
+    name: "TicTactoeWeb",
+    description: "Play TicTacToe against an unbeatable AI!",
+    github_link: "https://github.com/selimellieh72/TicTacToeWeb",
+    technology: "JavaScript",
+  },
+  {
+    name: "My Plans",
+    description:
+      "A cross-platform app to manage your plans created with Flutter and Hive.",
+    github_link: "https://github.com/selimellieh72/my-plans/",
+    technology: "Flutter",
+  },
+];
 export default function Projects() {
   return (
-    <section id='projects'>
-        <h2 className='animateOpacity'>Selected projects</h2>
-        <div className='projects__list'>
-        {PROJECTS.map((project, index) => <Project key={index} {...project} />)}
-        </div>
-
+    <section id="projects">
+      <h2 className="animateOpacity">Selected projects</h2>
+      <div className="projects__list">
+        {PROJECTS.map((project, index) => (
+          <Project key={index} {...project} />
+        ))}
+      </div>
     </section>
-  )
+  );
 }
